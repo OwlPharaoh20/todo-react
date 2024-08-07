d6aaff34246718e0964ec149372af0ce323da263
GG-Pharaoh <oxdiablo20@gmail.com>
2024-08-07 12:33:16 -0700

    updated features

---
+++ src/ToDoList.jsx
@@ -0,0 +1,79 @@
+import React, {useState} from 'react'
+
+function ToDoList () {
+
+    const [tasks, setTasks] = useState(["Eat Breakfast", "Take a shower", "Walk The Dog"]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event) {
+        setNewTask(event.target.value);
+
+    }
+ 
+
+    function addTask() {
+        setTasks(t =>[...t, newtask]);
+        setNewTask("");
+
+    }
+
+    function deleteTask (index) {
+
+
+    }
+
+    function moveTaskUp (index) {
+
+    }
+
+    function moveTaskDown (index) {
+
+    }
+
+
+
+    return (
+       <div className='to-do-list'> 
+        <h1> To-Do-List </h1>
+
+        <div> 
+            <input type="text"
+            placeholder="Enter a task..."
+            value={newTask}
+            onChange={handleInputChange}
+            />
+
+            <button className="add-button"
+            onClick={addTask}> 
+                Add Task 
+            </button>
+        </div>
+
+        <ol> 
+            {tasks.map((task, index)  => 
+            <li key={index}>
+                <span className='text'> {task} </span>
+                <button 
+                className="delete-button"
+                onClick={() =>deleteTask(index)}> 
+                    Delete 
+                </button>
+
+                <button 
+                className="move-button"
+                onClick={() =>moveTaskUp(index)}> 
+                    UP 
+                </button>
+                <button 
+                className="move-button"
+                onClick={() =>moveTaskDown(index)}> 
+                    Down 
+                </button> 
+            </li>)}
+        </ol>
+       </div>
+    );
+
+}
+
+export default ToDoList 
